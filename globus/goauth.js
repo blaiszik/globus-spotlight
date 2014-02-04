9b4800fea83a5fcbb7c3875017593185dce3aa8a
Blaiszik <blaiszik@uchicago.edu>
2014-02-04 15:30:44 -0600

    Morning commit

--- globus/goauth.js
+++ globus/goauth.js
@@ -1,11 +1,14 @@
 // requires jquery.cookie plugin
 $.ajaxPrefilter(function(options,originalOptions,jqXHR) {
+	es_url_length = es_url.length;
     token = $.cookie("globusonline-goauth");
-    console.log(options);
-    console.log(originalOptions);
-    console.log(jqXHR);
-    if (token) {
+
+    //Edited to only add tokens that are not sent to Elasticsearch
+    if (token && !(options.url.slice(0,es_url_length) == es_url) ) {
+    	console.log('adding headers');
         jqXHR.setRequestHeader("Authorization",
                                "Globus-Goauthtoken " + token);
+    }else{
+    	console.log('not adding headers');
     }
 });
