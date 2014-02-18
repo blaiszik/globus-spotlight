3c906e740f415c36d3bbb33abc18b52b4eb1b717
Blaiszik <blaiszik@uchicago.edu>
2014-02-18 11:00:27 -0600

    transfer integration

--- assets/spotlight.js
+++ assets/spotlight.js
@@ -193,7 +193,11 @@ function gs_perform_transfer(){
 
   debug = files_to_transfer;
   for(var propt in files_to_transfer){
-    console.log(propt + ': ' + files_to_transfer[propt]);
+        console.log(propt + ': ' + files_to_transfer[propt]);
+
+     for(member in files_to_transfer[propt]){
+      console.log(member);
+     } 
 }
 
   //go_transfer_file(ep1, ep2, files, label, callback);
