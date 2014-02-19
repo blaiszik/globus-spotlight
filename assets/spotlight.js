75016d295986aa9c7a826fd23a4d2c65235b805b
Blaiszik <blaiszik@uchicago.edu>
2014-02-19 16:18:33 -0600

    scripts to end of body

--- assets/spotlight.js
+++ assets/spotlight.js
@@ -140,6 +140,12 @@ function gs_load_tag_list(){
      data: JSON.stringify(requestData),
      success: function(data) {
       console.log(data);
+      //<label class='quick-tag font-white' style="color: #fff">Scattering</label> | 
+      tag_groups = data.facets.tags.term;
+      for(i=0;i<tag_groups.length; i++){
+        console.log(tag_groups[i]);
+      }
+
      }
    });
 }
